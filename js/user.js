2d7d918957dd6d35297323d1216646fe77bd5429
Admin <Admin>
2016-05-03 16:50:29 +0530

    using events

--- js/user.js
+++ js/user.js
@@ -45,24 +45,39 @@ var ViewRepo = Backbone.View.extend({
        }
       }); 
 var UserList = Backbone.View.extend({
-      el:'.page',
-        render:function(){
-        var that = this;
-          var users = new Users();
-          users.fetch({
-          success:function(users){
+    el:'.page',
 
-        var source = $('#user-list-template').html();
-        var template = Handlebars.compile(source);
-        var html = template(users.toJSON());
-          
-          /*var template = _.template($('#user-list-template').html(),{users:users.models});*/
-          
-          that.$el.html(html);
-          }
-        });
-        }
-      });
+    container: '#user-list-template table tbody',
+    tagName: 'tr',
+    events: {
+    'click #viewRepo' : "viewRepo",
+    },
+
+
+     viewRepo: function(e){
+      var id = $(e.currentTarget).data('id');
+                  //update url and pass true to execute route method
+               Backbone.history.navigate('repo/'+ id , true);
+              },
+
+        
+          render:function(){
+            var that = this;
+              var users = new Users();
+              users.fetch({
+              success:function(users){
+
+            var source = $('#user-list-template').html();
+            var template = Handlebars.compile(source);
+            var html = template(users.toJSON());
+              
+              /*var template = _.template($('#user-list-template').html(),{users:users.models});*/
+              
+              that.$el.html(html);
+              }
+            });
+            }
+          });
 var Router = Backbone.Router.extend({
         routes:{
         '': 'home',
